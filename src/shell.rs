02326ccfc29b49c15e73679a89215e3fb21e67d4
shsyss <103440382+shsyss@users.noreply.github.com>
2022-08-09 16:18:35 +0000

    Add description bash.txt

--- src/shell.rs
+++ src/shell.rs
@@ -1,10 +1,16 @@
 use std::{path::Path, process::Command};
 
 pub fn change_dir<P: AsRef<Path>>(path: P) -> anyhow::Result<()> {
-  let cmd_name = "_easychangedirectory";
+  // let cmd = "bash";
+  // let cmd_func = "_easychangedirectory";
+  // let path = path.as_ref().to_string_lossy().to_string();
+
+  // Command::new(cmd).args(&[cmd_func, &path]).output()?;
+
+  let cmd = "_easychangedirectory";
   let path = path.as_ref().to_string_lossy().to_string();
 
-  Command::new(cmd_name).arg(path).output()?;
+  Command::new(cmd).arg(&path).status()?;
 
   Ok(())
 }
