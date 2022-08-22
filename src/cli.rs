c3866f90e5df163d7ab5ce1b02154d6e60902d0b
shsyss <shsyss00@gmail.com>
2022-08-22 06:50:05 +0000

    Fix move key map description into program

--- src/cli.rs
+++ src/cli.rs
@@ -5,4 +5,24 @@ pub fn build_cli() -> Command<'static> {
     .arg(Arg::new("temp_path").hide(true).required(false))
     .arg(arg!(--init <SHELL> "Configure shell").required(false))
     .override_usage("ed")
+    .about(
+      "\
+ -------------------------------------------------------------
+|   Key         |    Description                              |
+|---------------|---------------------------------------------|
+| ↑ k           | Move up                                     |
+| ↓ j           | Move down                                   |
+| ← h           | Move parent directory                       |
+| → l           | Move Child directory                        |
+| Home	        | Move to top                                 |
+| End           | Move to bottom                              |
+| PageUp        | Skip a little and move up                   |
+| PageDown      | Skip a little and move down                 |
+| Enter c ;     | Change directory to current directory       |
+| Esc Ctrl+c q	| Exit and return to original directory       |
+| Insert Ctrl+s	| Search mode switch (Char key will not work) |
+| Backspace     | Delete one character from the search string |
+| Delete        | Delete all search strings                   |
+ -------------------------------------------------------------",
+    )
 }
