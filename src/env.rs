4457feb376ee662b7de0c90a3eb45ebbb4e6c637
shsyss <103440382+shsyss@users.noreply.github.com>
2022-09-04 16:44:44 +0900

    Improvement

--- src/env.rs
+++ src/env.rs
@@ -25,11 +25,11 @@ impl Config {
   }
 
   pub fn show_all(&self) {
-    println!("_ED_SET_BG = {}", self._ed_set_bg.map(|u| u.to_string()).unwrap_or("".to_string()));
-    println!("_ED_SHOW_INDEX = {}", self._ed_show_index.map(|u| u.to_string()).unwrap_or("".to_string()));
+    println!("_ED_SET_BG = {}", self._ed_set_bg.map(|u| u.to_string()).unwrap_or_else(|| "".to_string()));
+    println!("_ED_SHOW_INDEX = {}", self._ed_show_index.map(|u| u.to_string()).unwrap_or_else(|| "".to_string()));
     println!(
       "_ED_VIEW_FILE_CONTENTS = {}",
-      self._ed_view_file_contents.map(|u| u.to_string()).unwrap_or("".to_string())
+      self._ed_view_file_contents.map(|u| u.to_string()).unwrap_or_else(|| "".to_string())
     );
   }
 }
