2523627456cfbe547ce92ed3b8e5420b967eb585
shsyss <shsyss00@gmail.com>
2022-09-09 10:48:44 +0000

    Add support for zsh

--- src/shell.rs
+++ src/shell.rs
@@ -6,8 +6,8 @@ function ed() {
   if [[ "$#" -eq 0 ]]; then
     temp_path="{{ temp_path }}.$$"
     easychangedirectory "${temp_path}"
-    path=`cat "${temp_path}"`
-    cd "${path}" || return
+    cd_path=`cat "${temp_path}"`
+    cd "${cd_path}" || return
   elif [[ "$#" -eq 1 ]] && [[ "$1" =~ ^-+[a-zA-Z]+ ]]; then
     easychangedirectory "$1"
   elif [[ "$#" -eq 1 ]]; then
@@ -27,8 +27,8 @@ function ed
   if test "$arg_cnt" -eq 0
     set temp_path "{{ temp_path }}.$fish_pid"
     easychangedirectory "$temp_path"
-    set path (cat "$temp_path")
-    cd "$path"
+    set cd_path (cat "$temp_path")
+    cd "$cd_path"
   else if string match -r '^x\-+[a-zA-Z]+' "x$argv[1]" &> /dev/null
     easychangedirectory "$argv[1]"
   else if test "$arg_cnt" -eq 1
@@ -47,8 +47,8 @@ function ed {
   if ($args.Length -eq 0) {
     $temp_path = "{{ temp_path }}.$pid"
     easychangedirectory $temp_path
-    $path = (cat $temp_path)
-    cd $path
+    $cd_path = (cat $temp_path)
+    cd $cd_path
   } elseif ($args.Length -eq 1 -and $args[0] -match '^-+[a-zA-Z]+') {
     easychangedirectory $args[0]
   } elseif ($args.Length -eq 1) {
@@ -67,8 +67,8 @@ function ed() {
   if [[ "$#" -eq 0 ]]; then
     temp_path="{{ temp_path }}.$$"
     easychangedirectory "${temp_path}"
-    path=`cat "${temp_path}"`
-    cd "${path}" || return
+    cd_path=`cat ${temp_path}`
+    cd "${cd_path}" || return
   elif [[ "$#" -eq 1 ]] && [[ "$1" =~ ^-+[a-zA-Z]+ ]]; then
     easychangedirectory "$1"
   elif [[ "$#" -eq 1 ]]; then
