c18d3aaccbfb42a15c3ab06eba866443b3a0ed34
shsyss <103440382+shsyss@users.noreply.github.com>
2022-09-12 11:22:02 +0900

    Fix signature

--- src/connect.rs
+++ src/connect.rs
@@ -1,6 +1,6 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{fs::File, io::Write, path::Path};
 
-pub fn pipe_shell(path: &PathBuf, temp_path: &str) -> anyhow::Result<()> {
+pub fn pipe_shell(path: &Path, temp_path: &str) -> anyhow::Result<()> {
   let mut f = File::create(temp_path)?;
   f.write_all(path.to_string_lossy().as_bytes())?;
 
