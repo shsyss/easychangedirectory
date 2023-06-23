8016149c9d6a8eeb86d7804abffb412469119bc6
dijdzv <dijdzv@gmail.com>
2023-06-24 00:57:25 +0900

    Update clap to v4.3.5

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use clap::{arg, command, Arg, Command};
 
-pub fn build_cli() -> Command<'static> {
+pub fn build_cli() -> Command {
   command!()
     .arg(Arg::new("temp_path").hide(true).required(false))
     .arg(arg!(--init <SHELL> "Configure shell").required(false))
